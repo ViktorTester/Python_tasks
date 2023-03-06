4c553c40d3a4ce3bcc37b0861249d015a181ff3d
VIktor <viktor345@inbox.lv>
2023-03-06 11:31:58 +0200

    new task added

---
+++ easy/figure_area.py
@@ -0,0 +1,15 @@
+figure = input()
+if figure == 'triangle':
+    a, b, c = int(input()), int(input()), int(input())
+    p = (a + b + c) / 2
+    print((p * (p - a) * (p - b) * (p - c)) ** 0.5)
+elif figure == 'rectangle':
+    a, b = int(input()), int(input())
+    print(a * b)
+else:
+    r = int(input())
+    print(3.14 * (r ** 2))
+
+# The program finds the areas of some figures.
+# The input is the shape type (triangle, rectangle and circle)
+# and the corresponding parameters.
---
+++ easy/lucky_ticket.py
@@ -0,0 +1,19 @@
+S = input()
+S = int(S)
+
+D_1 = S // 100000
+D_2 = S // 10000 % 10
+D_3 = S // 1000 % 10
+D_4 = S // 100 % 10
+D_5 = S // 10 % 10
+D_6 = S % 10
+
+if D_1 + D_2 + D_3 == D_3 + D_4 + D_5:
+    print('Lucky')
+else:
+    print('Common')
+
+# The program finds out if the ticket is lucky.
+# The input to the program is a string of six digits.
+# A ticket is considered lucky if the sum of the first three digits matches
+# the sum of the last three digits of the ticket number.
